2f03a3355029c336191abfcfdf8ba4d4206a24ce
Narcis Fanica <narcis.fanica@vivacredit.ro>
2024-08-04 11:16:16 +0300

    Finished first article

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log('Hello from app.ts');
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/healthcheck", (_req, res) => {
+  res.send("API is up and running!");
+});
+
+app.listen(3000, () => {
+  console.log("App is running!");
+});
